8107cdf21b6886601d9b24a71018d930abb0b012
Fernando Costa <luff3@gmail.com>
2024-11-09 21:33:12 -0300

    fix: upsert transaction function

--- app/transactions/page.tsx
+++ app/transactions/page.tsx
@@ -3,10 +3,21 @@ import { DataTable } from "../_components/ui/data_table";
 import { transactionColumns } from "./_columns";
 import AddTransactionButton from "../_components/add-transaction-button";
 import Navbar from "../_components/navbar";
+import { auth } from "@clerk/nextjs/server";
+import { redirect } from "next/navigation";
 
 const TransactionsPage = async () => {
+  const { userId } = await auth();
+  if (!userId) {
+    redirect("/login");
+  }
+
   //acessar as transações do meu banco de dados
-  const transactions = await db.transaction.findMany({});
+  const transactions = await db.transaction.findMany({
+    where: {
+      userId,
+    },
+  });
   return (
     <>
       <Navbar />
